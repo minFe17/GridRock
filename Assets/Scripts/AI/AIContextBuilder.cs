f6c2c708f61a74dcf3da05feec5c79208bfbf09d
minFe17 <vvcx1234@naver.com>
2026-01-11 14:36:24 +0900

    블록 제작 AIContextBuilder 설계

---
+++ Assets/Scripts/AI/AIContextBuilder.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class AIContextBuilder : MonoBehaviour
+{
+    // 싱글턴
+
+    public AIContext BuildContext()
+    {
+        // PlayerContext, GridContext, BlockContext, AIStateContext를 여기서 수집해서 반환
+        return default;
+    }
+}
